7f3c6af1729b486f325b022dbdce35558c55121e
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2023-09-18 17:45:50 -0500

    Update and rename Unlimited-Saves.js to Unlimited-Saves.user.js

--- Unlimited-Saves.user.js
+++ Unlimited-Saves.user.js
@@ -3,7 +3,7 @@
 // @description Allows for an unlimited amount of saves on Pixlr.com
 // @version 2.0.0
 // @icon https://repository-images.githubusercontent.com/691387917/16f4c271-27e2-4e73-8cca-edc98d40304e
-// @updateURL https://raw.githubusercontent.com/Oshanotter/Unlimited-Saves/main/Unlimited-Saves.js
+// @updateURL https://raw.githubusercontent.com/Oshanotter/Unlimited-Saves/main/Unlimited-Saves.user.js
 // @namespace Oshanotter
 // @author Max Forst
 // @include https://pixlr.com/e/*
